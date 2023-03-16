d480a790f3a8e208558b555285444cda25fd55a7
Jacky Lau <>
2023-03-16 11:52:22 +1100

    Update to use notion client

--- pages/api/update-notion.js
+++ pages/api/update-notion.js
@@ -1,29 +1,26 @@
+import { Client } from "@notionhq/client"
+
+const notion = new Client({ auth: process.env.MY_NOTION_TOKEN })
+
 export default async function (req, res) {
   try {
-    await fetch('https://api.notion.com/v1/pages', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${process.env.MY_NOTION_TOKEN}`,
-        'Notion-Version': '2021-05-13',
-      },
-      body: JSON.stringify({
-        parent: { database_id: `${process.env.DATABASE_ID}` },
-        properties: {
-          Email: {
-            title: [
-              {
-                text: {
-                  content: `${req.body.email}`,
-                },
+    const response = await notion.pages.create({
+      parent: { database_id: process.env.DATABASE_ID },
+      properties: {
+        Email: {
+          title: [
+            {
+              text: {
+                content: `${req.body.email}`,
               },
-            ],
-          },
-        },
-      }),
+            },
+          ],
+        }
+      },
     })
+    console.log(response)
   } catch (err) {
-    console.log('Error', err)
+    console.error('Error', err)
   }
 
   res.status(200).json({ status: 'success' })
